a5134ea006889c3faa54dbd9263a655337384613
Renan Balter <renan_balter@hotmail.com>
2026-03-13 16:23:59 -0300

    created routes and implemented JWT and its configs.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+// JWT
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -46,4 +50,27 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
+
 }
---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('auth')->middleware('api')->group(function() {
+
+    Route::post('register', [AuthController::class, 'register' ]);
+    Route::post('login', [AuthController::class, 'login' ]);
+    Route::post('logout', [AuthController::class, 'logout' ]);
+    Route::post('refresh', [AuthController::class, 'refresh' ]);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
